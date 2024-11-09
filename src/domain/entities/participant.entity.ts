6d4abc34cdc0cf99b8754e0fd54d961ee47ad8f5
adelmnv <malgonussova@inbox.ru>
2024-11-09 18:33:20 +0500

    chore: create user, room, participant entity

---
+++ src/domain/entities/participant.entity.ts
@@ -0,0 +1,33 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Room } from "./room.entity";
+import { User } from "./user.entity";
+
+@Entity('participants')
+export class Participant{
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => Room, (room) => room.participants)
+    @JoinColumn()
+    room: Room;
+
+    @ManyToOne(() => User, (user) => user.participations)
+    @JoinColumn()
+    user: User;
+
+    @OneToOne(() => Participant, {nullable: true})
+    @JoinColumn()
+    participantReceiver: Participant | null;
+    
+    @Column({type: 'varchar'})
+    wishlist: string;
+    
+    @CreateDateColumn({type: 'timestamp'})
+    createdAt: Date;
+    
+    @UpdateDateColumn({type: 'timestamp'})
+    updatedAt: Date;
+    
+    @DeleteDateColumn({type: 'timestamp'})
+    deletedAt: Date | null;
+}
